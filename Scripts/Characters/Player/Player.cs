824b230e021acde8076a4c1c217a51221babe787
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-06 22:35:42 +0900

    Initial Commit

---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+  private Vector2 direction = new();
+  
+  public override void _PhysicsProcess(double delta)
+  {
+    Velocity = new(direction.X, 0, direction.Y);
+    Velocity *= 5;
+
+    MoveAndSlide();
+  }
+
+  public override void _Input(InputEvent @event)
+  {
+    direction = Input.GetVector(
+      "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+    );
+  }
+}
