0e448fb6b9ba9e7d645b43b28c8bdceda7c52f22
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-11 22:43:10 +0900

    Created Dash State

--- Scripts/General/GameConstants.cs
+++ Scripts/General/GameConstants.cs
@@ -1,7 +1,11 @@
 public class GameConstants
 {
+  // Animations
   public const string ANIM_IDLE = "Idle";
   public const string ANIM_MOVE = "Move";
+  public const string ANIM_DASH = "Dash";
+
+  // INputs
   public const string INPUT_MOVE_RIGHT = "MoveRight";
   public const string INPUT_MOVE_LEFT = "MoveLeft";
   public const string INPUT_MOVE_FORWARD = "MoveForward";
