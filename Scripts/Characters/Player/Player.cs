d272ce2836a3c0cfcd63d57815e824f20005dc80
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-11 12:57:13 +0900

    StateMachine

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,20 +3,35 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-  private Vector2 direction = new();
-  
+  [ExportGroup("Required Nodes")]
+  [Export] public AnimationPlayer animPlayerNode;
+  [Export] public Sprite3D spriteNode;
+  [Export] public StateMachine stateMachineNode;
+
+  public Vector2 direction = new();
+
   public override void _PhysicsProcess(double delta)
   {
     Velocity = new(direction.X, 0, direction.Y);
     Velocity *= 5;
 
     MoveAndSlide();
+
+    Flip();
   }
 
   public override void _Input(InputEvent @event)
   {
     direction = Input.GetVector(
-      "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+      GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD
     );
+  }
+
+  private void Flip()
+  {
+    bool isNotMovingHorizontaly = Velocity.X == 0;
+    if (isNotMovingHorizontaly) return;
+    bool isMovingLeft = Velocity.X < 0;
+    spriteNode.FlipH = isMovingLeft;
   }
 }
